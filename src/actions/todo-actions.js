f7157878271dc571d5df16d561b47c2ed4193653
mcpengelly <pengelly.mat@gmail.com>
2017-06-20 20:24:20 -0400

    implement todo actions and reducer

--- src/actions/todo-actions.js
+++ src/actions/todo-actions.js
@@ -1,1 +1,8 @@
-// todo implement todo-actions
+import * as types from './action-types';
+
+export const addTodo = (todo) => {
+	return {
+		type: type.ADD_TODO,
+		todo
+	};
+};
--- src/reducers/todo-reducer.js
+++ src/reducers/todo-reducer.js
@@ -1,1 +1,10 @@
-// implement todo-reducer
+import * as types from '../actions/action-types';
+
+export default (state = [], action) => {
+	switch (action.type) {
+		case types.ADD_TODO:
+			return [...state, Object.assign({}, action.todo)];
+		default:
+			return state;
+	}
+}
